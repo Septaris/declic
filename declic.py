f7ebc78001271b6c75f0d209cb84534d26b4bb00
Septaris/Babouh <sonny.lion@laposte.net>
2018-05-03 19:44:38 +0200

    Remove poppy dependency and add __all__ list

--- declic.py
+++ declic.py
@@ -35,8 +35,7 @@
 import argparse
 import inspect
 
-from poppy.core.generic.cache import CachedProperty
-
+__all__ = ['command', 'group', 'argument', 'Group', 'Command']
 
 # patch add_parser to allow use of pre-existing parser
 def custom_add_parser(self, name, **kwargs):
@@ -68,6 +67,36 @@ def custom_add_parser(self, name, **kwargs):
 
 
 argparse._SubParsersAction.add_parser = custom_add_parser
+
+
+class CachedProperty(object):
+    """
+    A descriptor to cache the execution of a property, and sending the computed
+    value again and again.
+    """
+
+    def __init__(self, method):
+        """
+        Store the wrapped method, and its name.
+        """
+        self.name = method.__name__
+        self.func = method
+
+    def __get__(self, instance, owner):
+        """
+        When getting the attribute the first time, the descriptor is called and
+        then compute the method (no arguments) as usual. Then the method in
+        the instance is replaced by an attribute containing the cached value.
+        """
+        # compute the method as usual
+        result = self.func(instance)
+
+        # replace the method by the attribute, avoiding to call the descriptor
+        # again !
+        setattr(instance, self.name, result)
+
+        # return the result
+        return result
 
 
 class Command(object):
