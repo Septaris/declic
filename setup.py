d814b2bc57ce4d4c5e1f82363730ddfd539d3408
Sonny Lion <sonny.lion@laposte.net>
2018-05-03 18:47:05 +0200

    Add source code

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='declic',
+    version='1.0.0',
+    py_modules=['declic'],
+    url='github',
+    license='MIT',
+    author='S. Lion',
+    author_email='sonny.lion@laposte.net',
+    description='Declic (DEcorator-oriented CLI Creator) is a tiny Python package for creating command line interfaces '
+                'using decorators. It was inspired by the click package and is based on argparse'
+)
